594e1cc6aa3926b8a19d340dcea01a3f372647eb
genbar <genbar@ya.ru>
2021-11-14 18:11:02 +0300

    Добавление общей фикстуры

--- fixture/group.py
+++ fixture/group.py
@@ -23,6 +23,15 @@ class GroupHelper:
         wd.find_element(By.NAME, "submit").click()
         self.return_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.driver
+        self.open_groups_page()
+        # select first group
+        wd.find_element(By.NAME, "selected[]").click()
+        # submit deletion
+        wd.find_element(By.NAME, "delete").click()
+        self.return_to_groups_page()
+
     def return_to_groups_page(self):
         wd = self.app.driver
         wd.find_element(By.LINK_TEXT, "group page").click()
