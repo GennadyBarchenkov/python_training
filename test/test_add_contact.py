1a303db60ab506a9e451b300daad8289da0e5c51
genbar <genbar@ya.ru>
2021-11-14 15:52:04 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,20 +11,20 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact(firstname="imya", middlename="otchestvo", lastname="familiya",
                                    nickname="prozvishe", title="zagolovok", company="kompaniya", address="adres",
                                    home_telephone="domashnii", mobile_telephone="mobilnii", work_telephone="rabochii",
                                    fax="faks", email="miloraz", email2="milodva", email3="milotri",
                                    homepage="domashnay_stranica", bday="1", bmonth="January", byear="2021", aday="5",
                                    amonth="December", ayear="2022", address2="eshe_odin_adres", phone2="houm"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
                                    address="", home_telephone="", mobile_telephone="", work_telephone="", fax="",
                                    email="", email2="", email3="", homepage="", bday="", bmonth="", byear="", aday="",
                                    amonth="", ayear="", address2="", phone2=""))
-    app.logout()
+    app.session.logout()
