17bb5093d730b5fbc937edf48b81c5ad1593ee2c
genbar <genbar@ya.ru>
2021-11-16 21:41:04 +0300

    Добавить проверки текущего состояния сессии и выполнения действий с учётом текущего состояния.

--- fixture/session.py
+++ fixture/session.py
@@ -15,6 +15,27 @@ class SessionHelper:
 
     def logout(self):
         wd = self.app.driver
-
         wd.find_element(By.LINK_TEXT, "Logout").click()
         wd.find_element(By.NAME, "user")
+
+    def is_logged_in(self):
+        wd = self.app.driver
+        return len(wd.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.driver
+        return wd.find_element(By.XPATH, "//div[@id=\'top\']/form/b").text == "("+username+")"
+
+    def ensure_logout(self):
+        wd = self.app.driver
+        if self.is_logged_in():
+            self.logout()
+
+    def ensure_login(self, username, password):
+        wd = self.app.driver
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
--- test/test_add_groups.py
+++ test/test_add_groups.py
@@ -4,6 +4,5 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="qwerty", header="qwerty", footer="qwerty"))
 
-
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
