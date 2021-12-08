c25ee08d1d6348d13fc414bf6b0cc1dfb82a58a7
genbar <genbar@ya.ru>
2021-12-08 17:06:58 +0300

    Здание 12. Реализовано кеширование при загрузке списков групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -17,8 +17,8 @@ def test_edit_first_contact(app):
                       amonth="January", ayear="2000", address2="edit16", phone2="edit17")
     contact.id = old_contacts[0].id
     app.contact.modify_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
@@ -43,8 +43,8 @@ def test_modify_contact_lastname(app):
     contact = Contact(lastname="New lastname")
     contact.id = old_contacts[0].id
     app.contact.modify_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
@@ -56,7 +56,7 @@ def test_modify_contact_bday(app):
     contact = Contact(bday="30")
     contact.id = old_contacts[0].id
     app.contact.modify_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
