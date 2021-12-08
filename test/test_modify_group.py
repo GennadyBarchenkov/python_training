f0443cd383fec96dfae33455a0cc40c2392110a8
genbar <genbar@ya.ru>
2021-12-08 17:59:04 +0300

    Реализована проверка сравнения всех групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,9 +5,13 @@ def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="test", footer="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="edit1", header="edit2", footer="edit3"))
+    group = Group(name="edit1", header="edit2", footer="edit3")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_name(app):
@@ -27,15 +31,23 @@ def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="New header"))
+    group = Group(header="New header")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(footer="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(footer="New footer"))
+    group = Group(footer="test")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
