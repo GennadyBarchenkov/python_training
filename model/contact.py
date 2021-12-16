b7441bb7b84647ad29d82312030d7b3f55a4a6b2
genbar <genbar@ya.ru>
2021-12-16 15:26:47 +0300

    Параметризованы тесты для создания контактов

--- model/contact.py
+++ model/contact.py
@@ -36,7 +36,7 @@ class Contact:
         self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
+        return "%s:%s:%s:%s" % (self.id, self.lastname, self.firstname, self.middlename)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and \
