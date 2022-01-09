018aad797959ade59a0ec1e833c740415be99aaa
genbar <genbar@ya.ru>
2022-01-09 19:14:39 +0300

    Задание 21. Переделаны тесты для проверки информации о контактах на главной странице

--- test/test_contact_from_home_page.py
+++ test/test_contact_from_home_page.py
@@ -1,18 +1,5 @@
-from random import randrange
 import re
 from model.contact import Contact
-
-
-def test_some_contact_from_home_page(app):
-    count_contacts = app.contact.get_contact_list()
-    index = randrange(len(count_contacts))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def test_all_contacts_from_home_page_vs_db(app, db):
