3a388c8650407985667444fe4ecfaadf351a8e99
genbar <genbar@ya.ru>
2022-01-09 19:10:23 +0300

    Задание 21. Переделаны тесты для проверки информации о контактах на главной странице

--- test/test_contact_from_home_page.py
+++ test/test_contact_from_home_page.py
@@ -1,5 +1,6 @@
 from random import randrange
 import re
+from model.contact import Contact
 
 
 def test_some_contact_from_home_page(app):
@@ -14,8 +15,25 @@ def test_some_contact_from_home_page(app):
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
+def test_all_contacts_from_home_page_vs_db(app, db):
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for i in range(len(contacts_from_home_page)):
+        assert contacts_from_home_page[i].firstname == clear_space(contacts_from_db[i].firstname)
+        assert contacts_from_home_page[i].lastname == clear_space(contacts_from_db[i].lastname)
+        assert contacts_from_home_page[i].address == clear_space(contacts_from_db[i].address)
+        assert contacts_from_home_page[i].all_emails_from_home_page == clear_space(merge_emails_like_on_home_page(
+            contacts_from_db[i]))
+        assert contacts_from_home_page[i].all_phones_from_home_page == clear_space(merge_phones_like_on_home_page(
+            contacts_from_db[i]))
+
+
 def clear(s):
     return re.sub("[() -]", "", s)
+
+
+def clear_space(s):
+    return re.sub(r'\s+$', "", s)
 
 
 def merge_emails_like_on_home_page(contact):
